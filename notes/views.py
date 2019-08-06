d03291088ad01a6e980364a4011013e117adfdfc
Carlos Eduardo Lores Hernández <carloseduardoloreshernandez@MacBook-Pro-de-Carlos-2.local>
2019-08-06 14:57:06 +0200

    todo funciona de momento

--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,12 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from rest_framework import generics
 
-from django.shortcuts import render
+from . import  models
+from . import serializers
 
-# Create your views here.
+class NoteList(generics.ListAPIView):
+    queryset = models.Note.objects.all()
+    serializer_class = serializers.NoteSerializer
+
+class NoteDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Note.objects.all()
+    serializer_class = serializers.NoteSerializer
